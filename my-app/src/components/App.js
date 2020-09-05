6153d14302a40308aa43ebdaa66fddb5295bfb3a
Kun Hwi Ko <kunko@seas.upenn.edu>
2020-09-05 13:57:34 -0400

    create map functions for notes

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../notes"
+
+
 
 function App() {
 	return <div>
 		<Header />
-		<Note />
+    {Notes.map(noteItem => 
+      <Note 
+        key = {noteItem.key}
+        title = {noteItem.title}
+        content = {noteItem.content}
+      />
+    )}
 		<Footer />
 	</div>
 }
---
+++ my-app/src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    key : 1,
+    title : "Note 1",
+    content : "1"
+  },
+  {
+    key : 2,
+    title : "Note 2",
+    content : "2"
+  },
+  {
+    key : 3,
+    title : "Note 3",
+    content : "3"
+  },
+  {
+    key : 4,
+    title : "Note 4",
+    content : "4"
+  },
+  {
+    key : 5,
+    title : "Note 5",
+    content : "5"
+  },
+]
+
+export default notes
